7ae10de54f611aa36fb50dde417c463bafb14ac7
rodrigodsn <rodrigo.barros.dsn@gmail.com>
2025-07-31 05:34:00 -0300

    feat: add database migration setup with Alembic and user table creation

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,6 +1,23 @@
+from dataclasses import asdict
+
+from sqlalchemy import select
+
 from fast_zero.models import User
 
 
-def test_create_user():
-    user = User(username='testuser', email='test@test.com')
-    assert user.username == 'testuser'
+def test_create_user(session, mock_db_time):
+    with mock_db_time(model=User) as time:
+        user = User(username='testuser', email='test@test.com')
+
+        session.add(user)
+        session.commit()
+
+        user = session.scalar(select(User).where(User.username == 'testuser'))
+
+    assert asdict(user) == {
+        'id': 1,
+        'username': 'testuser',
+        'email': 'test@test.com',
+        'created_at': time,
+        'updated_at': time,
+    }
